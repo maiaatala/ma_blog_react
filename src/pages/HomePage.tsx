99e8eb670bee5aa9b10da71342e1be71493da964
maiaatala <ana.atala@unemat.br>
2025-04-19 18:26:04 -0400

    test: deploy simple env variable test

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import DateDisplay from '../components/DateDisplay'
+import React from "react";
+import DateDisplay from "../components/DateDisplay";
 
 const HomePage: React.FC = () => {
-    return (
-        <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <h1 style={{ fontSize: '4em' }}>Hello world!</h1>
-            <DateDisplay />
-        </div>
-    )
-}
+  const testV = import.meta.env.VITE_TEST_ENV;
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1 style={{ fontSize: "4em" }}>Hello world!</h1>
+      {testV ?? "not found"}
+      <DateDisplay />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
